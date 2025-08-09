79b6620e8f03c90d2378bbe46f519a24c23cb8af
hyesome <yuhssn@gamil.com>
2025-08-09 11:11:20 +0900

    기본 데이터 형태 학습

---
+++ 1. variable.py
@@ -0,0 +1,34 @@
+x = "안녕"       # x 에 문자열 "안녕" 값을 대입
+print(x)
+print(type(x))
+
+x = 3           # x 에 정수형 3을 대입
+print(x)
+print(type(x))
+
+x = 3.5         # x 에 실수형 3.5 을 대입
+print(x)
+print(type(x))
+
+x = 3 + 5       # x 에 3 + 5 의 연산결과 8 을 대입
+print(x)
+print(type(x))
+
+x = 3 == 5      # 3 == 5 는 거짓이므로 Boolean 값 False를 x에 대입
+print(x)
+print(type(x))
+
+x = True        # x에 boolean 값 True 대입
+print(x)
+print(type(x))
+
+x = [1, 2, 3, 4, 5] # list
+print(x)
+print(x[0])  #index : 몇번째 있는가
+print(x[3])
+print(type(x))
+
+x = {'a':100, 'b':200}  #dictionary   key:value
+print(x)
+print(x['a'])
+print(type(x))
