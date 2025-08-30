347622a3a8e25884e747e4fd20988fc92951df54
hyesome <yuhssn@gamil.com>
2025-08-30 09:51:26 +0900

    배열 형태의 연산

--- 2. operator.py
+++ 2. operator.py
@@ -45,3 +45,15 @@ print("str_x + str_y:", str_z)
 
 str_z = str_x * 2
 print("str_x * 2", str_z)
+
+array_x = [3, 6]
+array_y = [4, 5]
+
+array_z = array_x + array_y
+print("array_x + array_y : ", array_z)
+
+array_z = array_x * 2
+print("array_x * 2", array_z)
+
+array_z = array_x * array_y[0] # 해당 인덱스에 있는 요소가 정수형일때만 가능
+print("array_x + array_y[0] : ", array_z)
