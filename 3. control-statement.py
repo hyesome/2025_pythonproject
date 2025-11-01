e332442c200c27f3de7c313523d2e2ea70549e02
hyesome <yuhssn@gamil.com>
2025-11-01 11:38:09 +0900

    while-statement

--- 3. control-statement.py
+++ 3. control-statement.py
@@ -1,4 +1,4 @@
-score = int(input("점수를 입력하세요."))
+score = 100 # int(input("점수를 입력하세요."))
 
 if score > 100 or score < 0:
     print("정상적인 점수 범위가 아닙니다.")
@@ -9,3 +9,17 @@ else:
         print("재시험 응시가 필요합니다.")
     else:
         print("불합격입니다.")
+
+# while True:
+#     user_input = input("값을 입력하세요 : ")
+#
+#     if user_input.lower() == "z":
+#         break
+
+input_number = int(input("숫자를 입력하세요."))
+index = 1
+
+while index <= input_number:
+    if index % 2 == 0:
+        print(index)
+    index = index + 2
