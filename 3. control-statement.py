5dafb252ada06629b2569b088078e7fb885aac4b
hyesome <yuhssn@gamil.com>
2025-11-22 11:08:41 +0900

    for문을 활용한 점수구하기, zip() 활용

--- 3. control-statement.py
+++ 3. control-statement.py
@@ -56,3 +56,44 @@ def repeat_function():
 
 # 함수 호출 방법
 # repeat_function()
+
+# arr = ['AA','BB','CC','DD']
+#
+# for i in arr:
+#     print(i)
+#
+
+#수학
+test = [{ 'answer': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4]},
+ { 'answer': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
+ { 'answer': [1, 5, 3, 4, 5, 1, 2, 3, 1, 2]},
+ { 'answer': [4, 3, 4, 4, 5, 3, 1, 2, 2, 4]},
+ { 'answer': [1, 3, 4, 4, 5, 3, 1, 2, 3, 2]},
+ {'answer': [1, 3, 5, 4, 5, 3, 1, 2, 3, 4]},]
+
+
+
+
+
+a = [1, 5, 2, 4, 5, 3, 1, 2, 3, 4]
+correct_answer = [1, 3, 2, 4, 5, 3, 1, 2, 3, 4]
+
+# 학생의 점수구하기 한 문항당 10점이라 가정.
+
+score = 0
+for (student, correct) in zip(a, correct_answer):
+    if student == correct:
+        score = score + 10
+print(score)
+
+# score = 100
+# for (student, correct) in zip(a, correct_answer):
+#     if student != correct:
+#         score = score - 10
+# print(score)
+#
+# score = 0
+# for i in range(len(a)):
+#     if a[i] == correct_answer[i]:
+#         score = score + 10
+# print(score)
